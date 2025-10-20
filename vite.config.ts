c88eca7c24817c441d9a47a406e66240ce1237e5
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-10-20 12:36:30 +0530

    adjusted package.json & vite.config.ts for live deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Study-Sync_frontend_live/',
   plugins: [react()],
   resolve: {
     alias: {
