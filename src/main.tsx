76d1d88980ea172535e3bb1b4ccb4aa7cca40f71
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-10-20 12:45:44 +0530

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.tsx";
-import Login from "./pages/Login.js";
+import Login from "./pages/Login.tsx";
 import Signup from "./pages/Signup.tsx";
 import ForgetPassword from "./pages/ForgetPassword.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Study-Sync_frontend_live">
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
-        <Route path="/app" element={localStorage.getItem("user") ? <App /> : <Navigate to="/login" />} />
+        <Route
+          path="/app"
+          element={
+            localStorage.getItem("user") ? <App /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
